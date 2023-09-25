67e05b55b959813ba602d72702d570fffdcd20ee
Rubén <98332317+marsi107@users.noreply.github.com>
2023-09-25 10:33:59 +0200

    create main containter structure

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className="bg-amazoneClone-bg">HomePage</div>
+    <div className="bg-amazoneClone-bg">
+        <div className="min-w-[1000px] max-w-[1500px] m-auto bg-purple-900">
+            HomePage
+        </div>        
+    </div>
   )
 }
 
