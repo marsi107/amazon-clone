e989d6c0fdd78ed90b938ae0bf97b9fe3b578d6e
Rubén <98332317+marsi107@users.noreply.github.com>
2023-09-26 11:57:57 +0200

    Add info and style for items in search page

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,7 +1,9 @@
-import { useSearchParams } from 'react-router-dom'
+import { useSearchParams, Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import { ProductDetails } from './'
 
 import { callAPI } from '../utils/CallApi'
+import { ES_CURRENCY } from '../utils/constants'
 
 const SearchResults = () => {
   const [searchParams] = useSearchParams();
@@ -34,14 +36,26 @@ const SearchResults = () => {
   }, [searchParams])
 
   return (
-    <div className="min-w-[1200px] max-w-[1300px] m-auto">
+    <div className="min-w-[1200px] max-w-[1300px] m-auto pt-4">
       {
         products &&
         products.map((product, key) => {
           return (
-            <div key={key}>
-              {product.title}
-            </div>
+            <Link key={key} to={`/product/${product.id}`}>
+              <div className="h-[250px] grid grid-cols-12 rounded mt-1 mb-1">
+                <div className="col-span-2 p-4 bg-gray-200">
+                  <img className="m-auto" src={product.image_small} alt="" />
+                </div>
+                <div className="col-span-10 bg-gray-50 border border-gray-100 hover:bg-gray-100">                  
+                  <div className="font-medium text-black p-2">
+                    <ProductDetails product={product} ratings={true} />
+                    <div className="text-xl xl:text-2xl p-1">
+                      {ES_CURRENCY.format(product.price)}
+                    </div>
+                  </div>
+                </div>                
+              </div>
+            </Link>            
           )
         })
       }
