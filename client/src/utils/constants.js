4b2b0c6cac6e919f72f7d18290a79565023d67ec
Rubén <98332317+marsi107@users.noreply.github.com>
2023-09-28 11:35:53 +0200

    Configure the server and the client so they can run on the same project

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,5 @@
 export const BASE_URL = "..";
+export const SERVER_URL= "http://localhost:5000"
+export const CLIENT_URL = "http://localhost:3000"
 export const GB_CURRENCY = Intl.NumberFormat("en-GB", {style: "currency", currency: "GBP"});
 export const ES_CURRENCY = Intl.NumberFormat("es-ES", {style: "currency", currency: "EUR"});
---
+++ server/server.js
@@ -0,0 +1,36 @@
+console.log("Hi all")
+require('dotenv').config()
+const express = require("express")
+const app = express()
+app.use(express.json())
+app.use(express.static("../"))
+
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
+const CLIENT_DOMAIN = 'http://localhost:3000';
+
+app.get("/", (req, res)=>{
+    res.json({"Server":"Server running on http://localhost:5000"})
+})
+
+app.post("/create-checkout-session", async (req, res) => {
+      //res.json({ url: "My url" })
+      console.log("Server reached")
+      console.log(process.env.STRIPE_PRIVATE_KEY)
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+            price: 'price_1Nuvk2DwetCnRMHE5sDpUK3N',
+            quantity: 1,
+          },
+        ],
+        mode: 'payment',
+        success_url: `${CLIENT_DOMAIN}?success=true`,
+        cancel_url: `${CLIENT_DOMAIN}?canceled=true`,
+      });
+    
+      res.json({url: session.url})
+  })
+
+
+app.listen(5000, () => {console.log("Server running on port 5000")})
